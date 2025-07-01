21a1a0d0da3f0a2fa470783767af33268bcfd54c
ignacio-montaldi <ignaciomontaldi@gmail.com>
2025-07-01 18:49:22 -0300

    TK003 - Crear componente Header con Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <>
+      <Header />
       <main className='min-h-screen'>
+
       </main>
     </>
   )
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,10 +2,16 @@ import { HiAtSymbol } from 'react-icons/hi'
 import './header.css'
 import { FaPhone } from 'react-icons/fa'
 import { HiMiniBars3 } from 'react-icons/hi2'
+import { useState } from 'react'
+import Menu from '../Menu/Menu'
+import { FaXmark } from 'react-icons/fa6'
 const Header = () => {
+
+  const [visible, setVisible] = useState<boolean>(false)
+
   return (
     <header className='fixed w-full'>
-        <span id="header-info" className='relative bg-tangerine flex justify-between items-center'>
+        <span id="header-info" className='bg-tangerine flex justify-between items-center'>
             <div id='header-info-content'>
               <span id="header-info-email" className='flex items-center'>
               <HiAtSymbol className='text-softWhite'/>
@@ -19,7 +25,8 @@ const Header = () => {
             <img src="/images/blanco-grises1.png" alt="M.A.No" />
         </span>
         <nav id="header-nav" className='bg-lilac text-softWhite flex'>
-            <HiMiniBars3 id='menu-btn' className='hidden'/>
+            {!visible ? <HiMiniBars3 id='menu-btn' className='hidden' onClick={() => setVisible(!visible)}/> : <FaXmark id='menu-btn' className='hidden' onClick={() => setVisible(!visible)}/>}
+            <Menu visible={visible} setVisible={setVisible}/>
             <a href='/'><h2 className='font-impact'>Dr. Fernando Juarez Cesca</h2></a>
             <p className='hidden'></p>
             <ul className='flex justify-between items-center'>
