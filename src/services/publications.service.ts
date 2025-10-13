d2d7f8a409cfc1446188723ca6baa6fca49a6aec
ignacio-montaldi <ignaciomontaldi@gmail.com>
2025-10-13 16:59:06 -0300

    Editado archivo del publications.service

--- src/services/publications.service.ts
+++ src/services/publications.service.ts
@@ -16,7 +16,7 @@ export async function getAllPublications() {
 
 export async function getPublicationByID(id:string) {
     try {
-        const response = await fetch(`${API_URL}/query/production?query=*%5B_type+%3D%3D+%22post%22+%26%26+_originalId+%3D%3D+%22${id}%22%5D+%7C+order%28fecha+desc%29%5B0%5D%7B%0A++_originalId%2C%0A++fecha%2C%0A++titulo%2C%0A++descripcionBreve%2C%0A++%22fotoUrl%22%3A+foto.asset-%3Eurl%0A%7D&perspective=drafts`)
+        const response = await fetch(`${API_URL}/query/production?query=*%5B_type+%3D%3D+%22post%22+%26%26+_originalId+%3D%3D+%22${id}%22%5D%5B0%5D%7B%0A++_originalId%2C%0A++fecha%2C%0A++titulo%2C%0A++descripcionBreve%2C%0A++descripcionCompleta%2C%0A++%22fotoUrl%22%3A+foto.asset-%3Eurl%0A%7D&perspective=drafts`)
         if(response.ok) {
             const data = await response.json();
             return data.result;
